4bc0f630837a581040bc34a256f5dd7b561f49ef
Luis Alberto Rodriguez <luis.alber86@gmail.com>
2019-05-14 20:42:27 +0200

    commit
    lambdas y modificacion de funciones primer nivel

---
+++ lambdas.py
@@ -0,0 +1,9 @@
+from p01 import sumaTodos
+
+cubo = lambda x: x**3
+cuadrado = lambda x: x*2
+
+print(sumaTodos(3,cubo))
+print(sumaTodos(3,cuadrado))
+
+#Lambda funcion anonima, creada para usarla en una funcion que admite funciones
--- p01.py
+++ p01.py
@@ -4,6 +4,10 @@ def normal(x):
 def cuadrado(y):
     return y * y
 
+def cubo(x):
+    return x**3 #operador de potencia ** (al cubo)
+
+#funciones de primer nivel: admite funciones como parametro de entrada
 def sumaTodos(limitTo, f):
     resultado = 0
     for i in range(limitTo+1):
@@ -11,5 +15,9 @@ def sumaTodos(limitTo, f):
     
     return resultado
 
-print(sumaTodos(100, normal))
-print(sumaTodos(3,cuadrado))
+#Para que no lo haga cuando importo funciones
+#Y solo se ejecuta cuando lo realice desde este archivo
+if __name__ == '__main__':
+    print(sumaTodos(100, normal))
+    print(sumaTodos(3,cuadrado))
+    print(sumaTodos(3,cubo))
