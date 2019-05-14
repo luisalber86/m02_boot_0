e137d016080a8ff4252857138b6f4e57d9ac79f8
Luis Alberto Rodriguez <luis.alber86@gmail.com>
2019-05-14 21:22:43 +0200

    Update p03_map_filter_reduce.py
    reduce

--- p03_map_filter_reduce.py
+++ p03_map_filter_reduce.py
@@ -1,7 +1,13 @@
+#importa reduce desde functools
+from functools import reduce
+
 #operador map
 lista = [1, 3, -1, 15, 9, 8]
 
 listaDoble = map(lambda x: x*2, lista)
 listaPares = filter(lambda x: x%2 == 0,lista)
+
+sumantorio = reduce(lambda  x, y: x + y, lista)
+suma100 = reduce(lambda x,y: x + y, range(101))
 
 #realizar list(listaDoble) para mostrar
