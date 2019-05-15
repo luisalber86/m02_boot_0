ae5960ad03c9b2ac85e5112f8ff5e3831eaac25b
Luis Alberto Rodriguez <luis.alber86@gmail.com>
2019-05-15 20:22:04 +0200

    Update objetos02.py
    creacion de objetos y subclases

--- objetos02.py
+++ objetos02.py
@@ -5,3 +5,51 @@ class Perro():
         self.nombre = nombre
         self.edad = edad
         self.peso = peso
+    
+    def ladrar(self): #debemos poner un parametro siempre
+        if self.peso >= 8:
+            print("GUAU, GUAU")
+        else:
+            print("guau, guau")
+            
+    #funcion standard por si hacemos print, no nos devuelva el lugar de memoria
+    def __str__(self):
+        return "- Mi Nombre es {}, tengo: {} años y un peso de {} Kg".format(self.nombre, self.edad, self.peso)
+ 
+ 
+ 
+#Subclase de Perro, hereda de Perro
+class PerroAsistencia(Perro):
+    def __init__(self, nombre, edad, peso, amo):
+        Perro.__init__(self, nombre, edad, peso)
+        self.amo = amo
+        self.__trabajando = False 
+        
+    #sobreescribir metodo __str__ (print)
+    def __str__(self):
+        return "- Mi Nombre es {}, tengo: {} años y un peso de {} Kg. Soy un perro de asistencia de {}. Estoy Trabajando?".format(self.nombre, self.edad, self.peso, self.amo, self.__trabajando)
+
+    def pasear(self):
+        print("ayudo a pasear a mi amo {}".format(self.amo))
+
+    def ladrar (self):
+        if self.__trabajando:
+            print("No puedo ladrar")
+        else:
+            Perro.ladrar(self) #necesario meter la instancia, ivocar metodo padre
+
+    #definir funcion(metodo) para cambiar self.__trabajando
+    def trabajando(self, valor = None):
+        if valor == None:
+            return self.__trabajando
+        else:
+            self.__trabajando = valor
+
+salchicho = Perro('Salchicho', 3, 40)
+juno = Perro('Juno', 8, 17)
+lola = Perro('Lola', 2, 3)
+perroguia = PerroAsistencia ('Perroguia', 4, 9, 'Luis')
+
+
+print(juno)
+print(perroguia)
