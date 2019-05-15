e5f9aedeb2a0789d668571608f722bf1643d4bb4
Luis Alberto Rodriguez <luis.alber86@gmail.com>
2019-05-15 18:21:26 +0200

    Update recursividad.py
    recursiva

--- recursividad.py
+++ recursividad.py
@@ -4,3 +4,12 @@ def retrocontador(num):
         retrocontador(num - 1)
 
 retrocontador(10)
+
+
+def sumatoriorecur(num):
+    if num != 0:
+        return num + sumatoriorecur(num-1)
+    else:
+        return 0
+    
+print(sumatoriorecur(3))
